ace03a5c350a8119a89a84e3ad027ccb25c3d8a2
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-02-17 22:58:25 +0530

    Time: 11 ms (82.04%), Space: 9.3 MB (53.80%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     
     static const int bias=1001;
-    // int memo[21][bias+2005];
+
+    int memo[21][3001];
     int dp(int n, int target, vector<int>& nums){
+        if(n==-1) return target==0;
+            
         
-        if(n==-1)return target==0;
+        if(memo[n][target+bias]!=-1)return memo[n][target+bias];
         
         int ans=0;
+        // take it //
+        if(nums[n]<=target)
+            ans += dp(n-1, target-nums[n], nums);
+        // leave it //
+        ans += dp(n-1, target, nums);
         
-        // if(memo[n][bias+target]!=-1)return memo[n][bias+target];
-        
-        // If we take nth index as +ve // 
-        ans += dp(n-1, target-nums[n], nums);
-        // Take nth index as -ve //
-        ans += dp(n-1, target+nums[n], nums);
-        
-        // return memo[n][bias+target]=ans;
-        return ans;
+        return memo[n][target+bias] = ans;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        // memset(memo, -1, sizeof memo);
+        memset(memo, -1, sizeof memo);
         
-        return dp(n-1, target, nums);
+        int sum= accumulate(nums.begin(), nums.end(), 0);
+        
+        if((target+sum)&1)return 0;
+        
+        int new_target = (target+sum)/2;
+        
+        return dp(n-1, new_target, nums);
     }
 };
