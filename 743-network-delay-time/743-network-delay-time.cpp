32b28be590934cca418e046f1f007a57ff2863f4
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-05-09 20:21:02 +0530

    Time: 174 ms (45.44%), Space: 40.1 MB (63.06%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,56 +1,53 @@
-#define pii  pair<int,int>
+#define pii pair<int,int> 
 #define inf 0x3f3f3f3f
-#define ff first
-#define ss second
+#define eb emplace_back
+#define mkp make_pair
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        map<int, vector<pii>> graph;
+        for(auto &x: times)
+            graph[x[0]].eb(mkp(x[1], x[2]));
         
-        // vector<int> vis(n+1,0);
-        // for(int i=0;i<=n;++i)
-        //     vis[i]=0;
         
-        // single source all pair shortest path - Dijsktra //
-        vector<int> dist(n+1, inf);
-        vector<int> ans(n+1, inf);
-
-        vector<vector<pii>> graph(n+1);
+        int src=k;
         
-        for(auto &x: times){
-            graph[x[0]].emplace_back(make_pair( x[2], x[1] ));
-        }
+        int vis[n+1];
+        for(int i=1;i<=n;++i)vis[i]=0;
+        int dist[n+1];
+        for(int i=1;i<=n;++i)dist[i]=inf;
         
-        int source=k;
+        dist[src]=0;
         
-        dist[source]=0;
-        ans[source]=0;
-        priority_queue<pii, vector<pii>, greater<pii> > pq; // {dist_from_source, node}
+        priority_queue<pii, vector<pii>, greater<pii>> pq; // {dist_from_src, node }
         
-        pq.push({0, source});
+        pq.push(mkp(0, src));
         
         while(!pq.empty()){
-            auto tp=pq.top();
+            int start = pq.top().second;
+            int dist_start = pq.top().first; // will be same as dist[start] //
             pq.pop();
             
-            int wt=tp.ff;            
-            int u=tp.ss;
-            for( auto v: graph[u] ){
-                if(dist[u] + v.ff < dist[v.ss]){
-                    dist[v.ss] = dist[u] + v.ff;
-                    pq.push({dist[v.ss], v.ss});
-                    ans[v.ss]=dist[v.ss]-dist[u]; // v.ff
+            if(vis[start])continue;
+            
+            vis[start]=1;
+            
+            for(auto x: graph[start]){
+                int v=x.first;
+                int wt=x.second;
+                if(dist_start+wt<dist[v]){
+                    dist[v] = dist_start + wt;
+                    pq.push({dist[v], v});
                 }
             }
         }
+        // return dist[3];
+        int ans=0;
         
-        int res=0;
-        // return dist[2];
-        for(int i=1;i<=n;++i){
-            res=max(res, dist[i]);
-        }
+        for(int i=1;i<=n;++i)
+            ans=max(dist[i], ans);
         
-        if(res==inf)return -1;
-        return res;
+        return (ans==inf)?-1:ans;
         
     }
 };
