ccae5bcb626f47d8d6714c3d995a47968085cc1c
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-05-29 20:42:54 +0530

    Time: 100 ms (37.31%), Space: 24.5 MB (21.27%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -19,21 +19,15 @@ public:
     
     void dfs(int i, int j){
         
-        if(not check(i, j))
-            return;
-        
-        if(grid[i][j] == 0 or vis[i][j])
-            return;
+        if(!grid[i][j] or vis[i][j])return;
         
         vis[i][j] = 1;
-        
         q.push(mkp(i, j));
-        dist[i][j] = 0;
         
-        dfs(i+1, j);
-        dfs(i-1, j);
-        dfs(i, j-1);
-        dfs(i, j+1);
+        if(check(i+1, j) and grid[i+1][j] and !vis[i+1][j])dfs(i+1, j);
+        if(check(i-1, j) and grid[i-1][j] and !vis[i-1][j])dfs(i-1, j);
+        if(check(i, j-1) and grid[i][j-1] and !vis[i][j-1])dfs(i, j-1);
+        if(check(i, j+1) and grid[i][j+1] and !vis[i][j+1])dfs(i, j+1);
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
@@ -43,10 +37,12 @@ public:
         memset(vis, 0, sizeof vis);
         bool flag=0;
         
-        for(int i=0;i<n;++i)
-            for(int j=0;j<n;++j)
-                dist[i][j]=inf;
-        // memset(qvis, 0, sizeof qvis);
+      for(int i=0;i<n;++i)
+        for(int j=0;j<n;++j)
+            dist[i][j]=inf;
+
+
+        
         for(int i=0;i<n;++i){
             for(int j=0;j<n;++j){
                 if(grid[i][j]){
@@ -58,50 +54,54 @@ public:
             }
             if(flag)break;
         }
-
+        
+        q.push(null_pair);
+        
         // q -> [1,2,3,4,null]
         /// the process all these until you find null //
         // [null,5,6,7,8]
         // find null add a null if the q is not empty else break
         
-
-        
+        // memset(dist, 0, sizeof dist);
+      
         // cout<<"q.size():"<<q.size()<<'\n';
         
         int len = 0 ;
-       
-        
-        dist[q.front().first][q.front().second] = 0;
-        
-        while(!q.empty()){
-
-            int i=q.front().first;
-            int j=q.front().second;
-            q.pop();
+        int qvis[101][101];
+        memset(qvis, 0, sizeof qvis);
+        while(q.size()){
             
-            int dx[] = {1, -1, 0, 0};
-            int dy[] = {0, 0, 1, -1};
-            
-            for(int k = 0; k < 4; k++) {
-                int x = i + dx[k];
-                int y = j + dy[k];
+            while(!q.empty() and q.front()!=null_pair){
                 
-                if(check(x, y) and !vis[x][y]) {
-                    vis[x][y] = 1;
-                    q.push({x, y});
-                    dist[x][y] = min(dist[x][y], dist[i][j] + 1);
-                }
+                int i=q.front().first;
+                int j=q.front().second;
+                q.pop();
+                qvis[i][j]=1;
+                
+                // cout<<i<<" "<<j<<"\n";
+                
+                dist[i][j] = min(dist[i][j], len);
+                
+                if(check(i+1, j)  and !qvis[i+1][j])q.push(mkp(i+1, j)), qvis[i + 1][j] = 1;
+                if(check(i-1, j)  and !qvis[i-1][j])q.push(mkp(i-1, j)), qvis[i - 1][j] = 1;
+                if(check(i, j+1)  and !qvis[i][j+1])q.push(mkp(i, j+1)), qvis[i][j + 1] = 1;
+                if(check(i, j-1)  and !qvis[i][j-1])q.push(mkp(i, j-1)), qvis[i][j - 1] = 1;
+
             }
-         
-        }
             
-         
+            q.pop();
+            len++;
+            
+            if(q.empty())break;
+            else q.push(null_pair);
+        }
+        
         int ans = inf;
         
         for(int i=0;i<n;++i){
             for(int j=0;j<n;++j){
                 // cout<<dist[i][j]<<" ";
-                if(grid[i][j]==1 and dist[i][j] > 0)
+                if(grid[i][j]==1 and vis[i][j]==0)
                     ans = min(ans, dist[i][j]);
             }
             // cout<<"\n";
