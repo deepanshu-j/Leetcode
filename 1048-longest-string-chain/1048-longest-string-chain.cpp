9429966bf2e4f29c79c13e0b62c34a6b14f6b958
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-06-16 03:04:51 +0530

    Time: 119 ms (65.99%), Space: 13 MB (97.96%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,52 +1,42 @@
 class Solution {
+    
+    bool check(string &a, string &b) {
+        
+        int len = a.size();
+        // 0 1 2 3 //
+        // a b c   //
+        // a b a c //
+
+        // the first place where they differ is 2 after that they are again equal //
+        int k;
+        for(k = 0; k<len; ++k){
+            if(a[k] != b[k])
+                break;
+        }
+
+        return (k == len) || ((k<len) && (a.substr(k) == b.substr(k+1)));
+    }
+    
 public:
     int longestStrChain(vector<string>& words) {
-        
-        int ans = 1;
-        
-        int n=words.size();
-        unordered_map<string, int> dp;
-        
-        for(auto &x: words) dp[x] = 1;
-        
+        int n = words.size();
         sort(words.begin(), words.end(), [](string &left, string &right){
            return (left.length()<right.length()); 
         });
-        
+        vector<int> dp(n, 1);
         for(int i=n-1;i>=0;--i){
             
-            // cout<<words[i]<<"\n";
             int len = words[i].length();
             for(int j=i+1;j<n;++j){
-                
-                if(words[j].length() != len+1 )continue;
-                
-                // check if i is predecssor of j //
-                bool ok=true;
-                
-                // 0 1 2 3 //
-                // a b c   //
-                // a b a c //
-                
-                // the first place where they differ is 2 after that they are again equal //
-                int k;
-                for(k = 0; k<len; ++k){
-                    if(words[i][k] != words[j][k])break;
-                }
-                
-                if(k<len) ok = (words[i].substr(k) == words[j].substr(k+1));
-        
-                // if(words[i]=="a"){
-                //     cout<<ok<<"\n";
-                // }
-                
-                 // update // 
-                if(ok)  dp[words[i]] = max(dp[words[i]], dp[words[j]]+1);   
+                if(words[j].length() == len )
+                    continue;
+                if(words[j].length() > len+1 )
+                    break;
+                if(check(words[i], words[j]))
+                    dp[i] = max(dp[i], 1 + dp[j]);
             }
             
-            ans = max(ans, dp[words[i]]);
         }
-        
-        return ans;
+        return *max_element(dp.begin(), dp.end());
     }
 };
