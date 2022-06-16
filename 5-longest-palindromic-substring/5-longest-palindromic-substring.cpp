520ad7854fc51ffdd2e6fa81fac543d1c1e01751
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-06-16 15:27:50 +0530

    Time: 937 ms (11.73%), Space: 11.6 MB (53.40%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
+    string s;
+    int memo[1001][1001];
+    
+    int dp(int i, int j){
+        if(i>j)return false;
+        if(i==j)return true;
+        if(i+1==j) return (s[i]==s[j]);
+        
+        if(memo[i][j]!=-1)return memo[i][j];
+        
+        if(s[i]==s[j])return memo[i][j] = dp(i+1, j-1);
+        
+        return memo[i][j] = false;
+    }
+    
     string longestPalindrome(string s) {
+        Solution::s = s;
+        int n=s.size();
+
+        memset(memo, -1, sizeof memo);
         
-        // expansion center //
-        
-        int n = s.length();
-        
-        int st=0,end=0;
-        int mx=1;
-        
-        for(int i=0; i<n; ++i){
-            int len=1;
-            while(i-len>=0 and i+len<n and s[i-len]==s[i+len]){
-                if(i+len-(i-len)+1 > mx ){
-                    st = i-len;
-                    end = i+len;
-                    mx = i+len-(i-len)+1;
+        int mx=1, x=0, y=0;
+        for(int i=0;i<n;++i){
+            for(int j=0; j<n; ++j){
+                if(dp(i, j)){
+                    if(j-i+1>mx){
+                        mx = j-i+1;
+                        x=i;
+                        y=j;
+                    }
                 }
-                ++len;
-            }
-        }
-        
-        for(int i=0; i<n; ++i){
-            int len=0;
-            while(i-len>=0 and i+1+len<n and s[i-len]==s[i+1+len]){
-                if(i+1+len-(i-len)+1 > mx ){
-                    st = i-len;
-                    end = i+1+len;
-                    mx = i+1+len-(i-len)+1;
-                }
-                ++len;
             }
         }
         
         string ans="";
-        for(int i=st;i<=end;++i)ans += s[i];
-        
+        for(int i=x;i<=y;++i)ans += s[i];
         return ans;
     }
 };
