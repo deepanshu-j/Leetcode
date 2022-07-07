59a667d87fce1fe04a5838392c82520f5c5d55e2
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-07-07 14:22:48 +0530

    Time: 3 ms (85.12%), Space: 6.5 MB (75.79%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    int n, m;
     string s1, s2, s3;
-    int memo[101][101];
+    int n, m;
+    int memo[201][201];
     bool dp(int i, int j){
-        if(i==n and j==m)return 1;
+        if(i==n and j==m)return true;
+    
+        if(memo[i][j]!=-1)return memo[i][j];
         
+        bool ans = 0;
         
-        if(i<n and j<m and memo[i][j]!=-1)return memo[i][j];
+        if(s1[i]==s3[i+j])ans|=dp(i+1, j);
+        if(s2[j]==s3[i+j])ans|=dp(i, j+1);
         
-        int ans=0;
-        if(i<n and s1[i]==s3[i+j]){
-            ans |= dp(i+1, j);
-        }
-        if(j<m and s2[j]==s3[i+j]){
-            ans |= dp(i, j+1);
-        }
-        
-        return memo[i][j]=ans;
+        return memo[i][j] = ans;
     }
     
-    bool isInterleave(string s11, string s22, string s33) {
-        
-        s1=s11;
-        s2=s22;
-        s3=s33;
+    bool isInterleave(string s1, string s2, string s3) {
+        Solution::s1 = s1;
+        Solution::s2 = s2;
+        Solution::s3 = s3;
         
         memset(memo, -1, sizeof memo);
-        
-        n=s1.length(), m=s2.length();
-        if(n+m!=s3.length())return 0;
-        
-        return dp(0,0);
+        n = s1.length(), m = s2.length();
+        if(n+m!=s3.length())return false;
+        return dp(0, 0);
     }
 };
