85bbe6ea1733a561667a46926a634c6dfd55ad33
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-07-21 04:13:21 +0530

    Time: 46 ms (79.28%), Space: 19 MB (66.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,35 +1,21 @@
-#define vi vector<int> 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-        int inf = 0x3f3f3f3f;
-        sort(a.begin(), a.end(),[](vi &l, vi &r){
-            if(l[1]==r[1])return l[0]<r[0];
-                return l[1]<r[1];
+        sort(a.begin(), a.end(), [](vector<int>& l, vector<int>& r){
+            if(l[0]==r[0])return l[1]<r[1];
+            return l[0]<r[0];
         });
         
+        vector<vector<int>> merged{a[0]};
+        
         int n = a.size();
-        
-        vector<vi> ans;
-        
-        vi curr = {inf, inf};
-        
-        for(int i=n-1; i>=0; --i){
-            
-            if(a[i][1]>=curr[0] and a[i][1]<=curr[1]){
-                
-                curr[0] = min(curr[0], a[i][0]);
-                
+        for(int i=0; i<n; ++i){
+            if(a[i][0] > merged.back()[1]){
+                merged.emplace_back(a[i]);
             }else{
-                ans.push_back(curr);
-                curr = a[i];
+                merged.back()[1] = max(a[i][1], merged.back()[1]);
             }
         }
-        if(ans.back()!=curr)ans.push_back(curr);
-        
-        sort(ans.begin(), ans.end());
-        ans.pop_back();
-        
-        return ans;
+        return merged;
     }
 };
