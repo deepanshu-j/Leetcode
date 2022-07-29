444558abe8d8be4e654ab6ab03b3d47a3925f6a2
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-07-30 03:14:47 +0530

    Time: 260 ms (51.86%), Space: 18.7 MB (22.20%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,65 +1,49 @@
-#define rep(i,a,b) for(int i=a ; i<(b) ; ++i )
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-typedef vector<int> vi;
-
 class Solution {
 public:
     
-    int sum=0;
-    int sz=-1;
-    bool res=false;
-    void dp(int n, vi &psets, int &k, vi &nums){
-        // base case //
-        if(n==sz){
-            bool ok=1;
-            rep(i,0,psets.size())
-                if(psets[i]!=sum) ok=false;   
-            if(psets.size()!=k)ok=false;
-            
-            if(ok){
-                res=true;
+    int n;
+    vector<int> nums;
+    int targetSum;
+    int memo[16][1<<16+1];
+    // kth bucket you're on and current state of taken elements
+    bool dp(int k, int mask, int sum){
+        
+        if(k==0)return ( mask == (1<<n)-1 );
+        
+        if(memo[k][mask]!=-1)return memo[k][mask];
+        
+        bool ans = 0;
+        for(int i=0;i<n;++i){
+            if((mask&(1<<i)) == 0){
+                
+                // take it // 
+                sum -= nums[i];
+                
+                if(sum == 0){
+                    ans |= dp(k-1, mask | (1<<i), targetSum );
+                }else
+                if(sum > 0)
+                    ans |= dp(k,  mask | (1<<i), sum );
+                
+                sum += nums[i];
             }
-            
-            return;
         }
         
-        // recc //
-        // try to put this nth element in all the sets we have created or add into a new set(if possible)
-        
-        if(res)return;
-        
-        
-        // add it to some existing set //
-        rep(i,0,psets.size()){
-            
-            if(psets[i]+nums[n] <= sum){
-                psets[i] += nums[n];
-                dp(n+1, psets, k, nums);
-                psets[i] -= nums[n];
-            }
-            
-        }
-        // add into a new set if possible 
-        if(psets.size()<k){
-            psets.eb(nums[n]);
-            dp(n+1, psets, k, nums);
-            psets.pop_back();
-        }
-        
+        return memo[k][mask] = ans;
     }
     
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        vector<int> psets; // partioned sets //
-        int tot_sum = accumulate(all(nums),0);
-        if(tot_sum%k!=0)
-            return false;
         
-        sum = tot_sum/k;
-        sz = nums.size();
+        this->nums = nums;
+        this->n = nums.size();
         
-        dp(0, psets, k, nums);
+        this->targetSum = accumulate(nums.begin(), nums.end(), 0);
+        if(targetSum%k != 0)return false;
+        targetSum /= k;  
         
-        return res;
+        memset(memo, -1, sizeof memo);
+        
+        return dp(k, 0, targetSum );
+        
     }
 };
