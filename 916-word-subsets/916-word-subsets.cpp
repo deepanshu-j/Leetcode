a7b8b37910c56308e5481141d66481b6aa5bca0b
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-07-30 22:38:21 +0530

    Time: 178 ms (92.14%), Space: 57.4 MB (90.79%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         
-        map<char,int> requirement;
+        int requirement[26] = {0};
         
         for(auto &word: words2){
             
-            map<int, int> mp;
+            int mp[26] = {0};
             
             for(auto &ch: word){
-                mp[ch]++;
-                requirement[ch] = max(requirement[ch], mp[ch]);
+                mp[ch-'a']++;
+                requirement[ch-'a'] = max(requirement[ch-'a'], mp[ch-'a']);
             }
             
         }
@@ -23,15 +23,15 @@ public:
         
         for(string &word: words1){
             
-            map<char, int> now;
+            int now[26] = {0};
             
             for(auto &ch: word)
-                now[ch]++;
+                now[ch-'a']++;
             
             bool ok = true;
-            for(auto &[el, cnt]: requirement){
+            for(int i=0;i<26;++i){
                 
-                if(now[el]<cnt){
+                if(now[i]<requirement[i]){
                     ok = false;
                     break;
                 }
