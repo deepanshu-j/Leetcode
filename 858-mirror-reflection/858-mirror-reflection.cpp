35d5641b954fe92b62a79cf2f93cb2d42cc585d7
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-08-05 02:48:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        
+        int m = 1, n = 1;
+        while(m * p != n * q){
+            n++;
+            m = (n*q) / p;
+        }
+        
+        if (m % 2 == 1 && n % 2 == 0) 
+            return 2;
+        if (m % 2 == 1 && n % 2 == 1) 
+            return 1;
+         
+        return 0;
+        
+    }
+};
