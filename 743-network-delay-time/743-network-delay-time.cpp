6248f45bc9a4c789561b1a415c6fb636717409f3
Deepanshu Johar <55355645+deepanshu-j@users.noreply.github.com>
2022-09-03 05:35:15 +0530

    Time: 120 ms (97.06%), Space: 40.3 MB (57.34%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,26 +1,60 @@
+using pii = pair<int,int>;
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        int inf = 1e7;
+        // form adjList //
+        vector<vector<pii>> adjList(n+1);
+        for(auto &x: times){
+            
+            int u = x[0], v = x[1], w = x[2];
+            adjList[u].push_back({w, v});
+            
+        }
+        
+        // dijkstra //
+        const int inf = 1e7;
         int dist[n+1];
-        for(int i=0;i<=n;++i) dist[i] = inf;
+        for(int i=0;i<n+1;++i)dist[i] = inf;
         
         dist[k] = 0;
         
-        for(int i=0;i<n;++i){
-            for(auto &x: times){
-                int u = x[0], v = x[1], w = x[2];
-                dist[v] = min(dist[u] + w, dist[v]);
+        multiset<pii> ms; // {weight, node} //
+            
+        ms.insert({0, k});
+        
+        while(!ms.empty()){
+            
+            
+            // extract the smallest dist node from the source node //
+            pii top = *ms.begin();  
+            ms.erase(ms.begin());
+            int cur = top.second;
+            
+            // update the dist of all the adjacent nodes of this cur //
+            
+            for(pii adj: adjList[cur]){
+                
+                int weight = adj.first, adj_node = adj.second; 
+                if( dist[cur] + weight < dist[adj_node] ){
+                    int new_dist =  dist[cur] + weight;
+                    dist[adj_node] = min(dist[adj_node], new_dist); 
+                    ms.insert({ new_dist, adj_node }); 
+                }
+                
             }
+            
+            
         }
         
         
+        
         int mx = 0;
-        for(int i=1;i<=n;++i)
-            mx = max(mx, dist[i]);  
+        for(int i=1;i<=n;++i)mx = max(mx, dist[i]);
         
         if(mx == inf)return -1;
         return mx;
+        
+        
     }
 };
